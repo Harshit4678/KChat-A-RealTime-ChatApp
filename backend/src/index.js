8be8695c0de1d73e76872019384d404f544d412c
Harshit k <harshitkumar2045@gmail.com>
2025-08-07 00:55:38 +0530

    update4

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://klikchat-2025.vercel.app"
+      "https://klikchat-2025.vercel.app",
       "https://k-chat-admin-panel.vercel.app",
     ],
     credentials: true,
