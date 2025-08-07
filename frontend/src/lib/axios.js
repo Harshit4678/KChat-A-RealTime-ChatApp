73c7abd25e59fdedce432bcad069c1112a01f891
Harshit k <harshitkumar2045@gmail.com>
2025-08-08 03:01:48 +0530

    bug resolved

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosIntance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://kchat-a-realtime-chatapp.onrender.com",
   withCredentials: true,
 });
