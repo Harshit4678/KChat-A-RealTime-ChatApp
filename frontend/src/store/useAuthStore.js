4424f2ba8dcb7e3863ab755f3fada09319496aa9
Harshit k <harshitkumar2045@gmail.com>
2025-08-09 05:24:12 +0530

    bug fixed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,9 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://kchat-a-realtime-chatapp.onrender.com/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
