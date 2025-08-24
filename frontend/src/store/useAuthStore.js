f5c3e7f0e1d0c40266f8f735e69f357ccb3a4e7f
Harshit k <harshitkumar2045@gmail.com>
2025-08-24 16:16:34 +0530

    bug fixed 2

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000"
-    : "https://kchat-a-realtime-chatapp.onrender.com";
+    : "https://kchat-a-realtime-chatapp-production.up.railway.app";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
